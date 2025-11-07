3856e765d83f6489377fb6cc6c9d065d25612dad
Mattéo RENAUD <mrenaud2@cfai-formation.fr>
2025-11-07 13:54:48 +0100

    chore: ajout des éléments pour déploiement API sur Render

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ db.sequelize.sync()
 require("./routes")(app);
 
 // set port, listen for requests
-const PORT =  /*443*/ 3000;
+const PORT =  443;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
